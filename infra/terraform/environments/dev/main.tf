97da4fbc5a38931233d90a8c5b8d5ddd74e5ce6d
gguip1 <gguip7554@naver.com>
2026-01-20 08:55:29 +0900

    feat: dev 환경에 EC2 app 인스턴스 구성 추가
    ec2-single 모듈 호출 및 관련 변수 정의.

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -7,3 +7,19 @@ module "vpc" {
   vpc_subnet_cidrs   = var.vpc_subnet_cidrs
   vpc_tags           = var.vpc_tags
 }
+
+module "app_instance" {
+  source = "../../modules/ec2-single"
+
+  name             = "${var.vpc_name}-app"
+  vpc_id           = module.vpc.vpc_id
+  subnet_id        = module.vpc.subnet_ids[var.ec2_subnet_index]
+  tags             = var.vpc_tags
+  ami_id           = var.ec2_ami_id
+  instance_type    = var.ec2_instance_type
+  root_volume_size = var.ec2_root_volume_size
+  root_volume_type = var.ec2_root_volume_type
+  key_name         = var.ec2_key_name
+  ssh_cidrs        = var.ec2_ssh_cidrs
+  http_cidrs       = var.ec2_http_cidrs
+}
--- infra/terraform/environments/dev/variables.tf
+++ infra/terraform/environments/dev/variables.tf
@@ -23,3 +23,51 @@ variable "vpc_subnet_cidrs" {
   description = "A list of CIDR blocks for the VPC subnets"
   type        = list(string)
 }
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type for the single host"
+  type        = string
+  default     = "t4g.small"
+}
+
+variable "ec2_root_volume_size" {
+  description = "Root EBS volume size in GiB"
+  type        = number
+  default     = 30
+}
+
+variable "ec2_root_volume_type" {
+  description = "Root EBS volume type"
+  type        = string
+  default     = "gp3"
+}
+
+variable "ec2_ami_id" {
+  description = "Override AMI ID (optional)"
+  type        = string
+  default     = null
+}
+
+variable "ec2_key_name" {
+  description = "EC2 key pair name for SSH (optional)"
+  type        = string
+  default     = null
+}
+
+variable "ec2_subnet_index" {
+  description = "Subnet index to place the instance"
+  type        = number
+  default     = 0
+}
+
+variable "ec2_ssh_cidrs" {
+  description = "CIDR blocks allowed to access SSH"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "ec2_http_cidrs" {
+  description = "CIDR blocks allowed to access HTTP/HTTPS"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
