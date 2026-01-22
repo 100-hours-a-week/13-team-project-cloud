722db7990f95e3373013e878a726e05a446a7162
gguip1 <gguip7554@naver.com>
2026-01-22 20:31:16 +0900

    refactor: dev 환경에서 새 EC2 모듈 사용하도록 업데이트

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -9,17 +9,19 @@ module "vpc" {
 }
 
 module "app_instance" {
-  source = "../../modules/ec2-single"
+  source = "../../modules/ec2"
 
-  name             = "${var.vpc_name}-app"
-  vpc_id           = module.vpc.vpc_id
-  subnet_id        = module.vpc.subnet_ids[var.ec2_subnet_index]
-  tags             = var.vpc_tags
-  ami_id           = var.ec2_ami_id
-  instance_type    = var.ec2_instance_type
-  root_volume_size = var.ec2_root_volume_size
-  root_volume_type = var.ec2_root_volume_type
-  key_name         = var.ec2_key_name
-  ssh_cidrs        = var.ec2_ssh_cidrs
-  http_cidrs       = var.ec2_http_cidrs
+  name                         = "${var.vpc_name}-app"
+  vpc_id                       = module.vpc.vpc_id
+  subnet_id                    = module.vpc.subnet_ids[var.ec2_subnet_index]
+  tags                         = var.vpc_tags
+  ami_id                       = var.ec2_ami_id
+  instance_type                = var.ec2_instance_type
+  root_volume_size             = var.ec2_root_volume_size
+  root_volume_type             = var.ec2_root_volume_type
+  associate_public_ip_address  = var.ec2_associate_public_ip_address
+  eip_allocation_id            = var.ec2_eip_allocation_id
+  key_name                     = var.ec2_key_name
+  ssh_cidrs                    = var.ec2_ssh_cidrs
+  http_cidrs                   = var.ec2_http_cidrs
 }
