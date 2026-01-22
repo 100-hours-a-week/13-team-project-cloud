d951b60b07e8f019b262f909f3295b8589af6e3f
gguip1 <gguip7554@naver.com>
2026-01-22 19:56:33 +0900

    refactor: EC2 모듈을 ec2-single에서 ec2로 재구성

---
+++ infra/terraform/modules/ec2/sg.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "app" {
+  name        = "${var.name}-sg"
+  description = "Allow SSH/HTTP/HTTPS"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ssh_cidrs
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.http_cidrs
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.http_cidrs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    { Name = "${var.name}-sg" },
+    var.tags
+  )
+}
--- infra/terraform/modules/ec2/variables.tf
+++ infra/terraform/modules/ec2/variables.tf
@@ -16,53 +16,44 @@ variable "subnet_id" {
 variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
-  default     = {}
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t4g.small"
 }
 
 variable "root_volume_size" {
   description = "Root EBS volume size in GiB"
   type        = number
-  default     = 30
 }
 
 variable "root_volume_type" {
   description = "Root EBS volume type"
   type        = string
-  default     = "gp3"
 }
 
 variable "key_name" {
   description = "EC2 key pair name for SSH (optional)"
   type        = string
-  default     = null
 }
 
 variable "ssh_cidrs" {
   description = "CIDR blocks allowed to access SSH"
   type        = list(string)
-  default     = ["0.0.0.0/0"]
 }
 
 variable "http_cidrs" {
   description = "CIDR blocks allowed to access HTTP/HTTPS"
   type        = list(string)
-  default     = ["0.0.0.0/0"]
 }
 
 variable "associate_public_ip_address" {
   description = "Whether to associate a public IP"
   type        = bool
-  default     = true
 }
 
 variable "ami_id" {
   description = "Override AMI ID (optional)"
   type        = string
-  default     = null
 }
