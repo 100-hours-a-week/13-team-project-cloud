e4f49b8bc8b8df35c80833ce11c7f02a772370be
gguip1 <gguip7554@naver.com>
2026-01-23 20:56:48 +0900

    feat: dev-core 환경 추가 (EIP, DB EBS 볼륨 관리)

---
+++ infra/terraform/environments/dev-core/main.tf
@@ -0,0 +1,47 @@
+resource "aws_eip" "app" {
+  domain = "vpc"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = merge(
+    { Name = "${var.core_name}-eip" },
+    var.core_tags
+  )
+}
+
+resource "aws_ebs_volume" "db" {
+  availability_zone = var.db_availability_zone
+  size              = var.db_volume_size
+  type              = var.db_volume_type
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = merge(
+    { Name = "${var.core_name}-db" },
+    var.core_tags
+  )
+}
+
+output "eip_allocation_id" {
+  description = "Elastic IP allocation ID"
+  value       = aws_eip.app.id
+}
+
+output "eip_public_ip" {
+  description = "Elastic IP public IP"
+  value       = aws_eip.app.public_ip
+}
+
+output "db_volume_id" {
+  description = "DB EBS volume ID"
+  value       = aws_ebs_volume.db.id
+}
+
+output "db_availability_zone" {
+  description = "DB EBS availability zone"
+  value       = aws_ebs_volume.db.availability_zone
+}
