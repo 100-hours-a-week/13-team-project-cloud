a0eb9db07ecfcd43ef3002204a147aeef52fbe3a
gguip1 <gguip7554@naver.com>
2026-02-02 00:51:50 +0900

    feat: prod 환경 S3 원격 백엔드 설정 추가

--- infra/terraform/backend/prod/main.tf
+++ infra/terraform/backend/prod/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "moyeo-bab-tfstate-prod"
+  bucket = "moyeo-bab-tfstate-dev"
 
   lifecycle {
     prevent_destroy = true
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "moyeo-bab-tf-lock-prod"
+  name         = "moyeo-bab-tf-lock-dev"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
