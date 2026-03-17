44f9c0c6dc6c95199c93fbc3312f993e909074c7
gguip1 <gguip7554@naver.com>
2026-03-17 14:23:46 +0900

    feat: chatbot 서비스 K8s 매니페스트 및 ArgoCD Application 추가

--- v3-kubernetes/terraform/modules/ecr/variables.tf
+++ v3-kubernetes/terraform/modules/ecr/variables.tf
@@ -4,5 +4,5 @@ variable "common_tags" { type = map(string) }
 variable "service_names" {
   description = "ECR 레포지토리를 생성할 서비스 이름 목록"
   type        = list(string)
-  default     = ["backend", "recommend"]
+  default     = ["backend", "recommend", "chatbot"]
 }
